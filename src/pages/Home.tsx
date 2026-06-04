1b7af78c6d6ffb351b927b9ef5c8a4202c0f3f78
nextplaytv1 <nextplaytv1@gmail.com>
2026-06-04 16:11:28 -0300

    feat: replace Elfsight with Shapo reviews widget

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -335,7 +335,7 @@ export default function Home() {
             </h2>
           </div>
           <div className="mt-12 w-full min-h-[400px]">
-            <div className="elfsight-app-01833423-a7d8-4e0a-976d-e4bd4bf76747" data-elfsight-app-lazy></div>
+            <div id="shapo-widget-8400582cccb4fa801cff"></div>
           </div>
         </div>
       </section>
